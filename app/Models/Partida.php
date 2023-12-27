9b621146573ddd4122b5cdff3da0a060706b8877
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-26 21:19:37 -0300

    Update Partida.php
    adicionado os campos da ta bela

--- app/Models/Partida.php
+++ app/Models/Partida.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Partida extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'acertos',
+        'erros',
+        'data_hora',
+    ];
 }
