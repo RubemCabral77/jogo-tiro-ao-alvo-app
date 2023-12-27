6672473e898ccb99e8665c3d8ca201df66ef3bba
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-26 21:26:36 -0300

    Update PartidaController.php
    criado a função de create partida

--- app/Http/Controllers/PartidaController.php
+++ app/Http/Controllers/PartidaController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Partida;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PartidaController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $userID = Auth::user()->id;
+        $partida = new Partida([
+            'acertos' => $request->acertos,
+            'erros' => $request->erros,
+            'data_hora' => date('d-m-Y H:m:s'),
+        ]);
+
+        $partida['user_id'] = $userID;
+
+        $partida->save();
+
+        session()->flash('msg', ['tipo' => 'success', 'text' => 'Progresso salvo, verifique o ranking']);
+
+        // return redirect('/pagina_a_definir');
+    }
 }
