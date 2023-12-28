f24167ca02a266c1938f3709dc7a01ff0744e1d8
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-28 20:04:00 -0300

    Update navigation.blade.php
    adicionado nav link do jogo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,15 +5,15 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('jogo.show') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('jogo.show')" :active="request()->routeIs('dashboard')">
+                        {{ __('Jogo') }}
                     </x-nav-link>
                     <x-nav-link :href="route('partida.show')" :active="request()->routeIs('partida.show')">
                         {{ __('Ranking') }}
