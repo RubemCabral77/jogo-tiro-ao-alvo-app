581e9ec89481ed9ca7ed49957001c7034794e548
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-28 07:48:15 -0300

    Create punctuation.blade.php
    criação da parte da pontuação da view jogo

---
+++ resources/views/jogo/partials/punctuation.blade.php
@@ -0,0 +1,14 @@
+<div class="classificacao row mt1 mb1 espaco-ao-redor">
+    <div class="box bg-principal">
+        <h2 class="col text-center">Acertos: <span id="acertos">0</span></h2>
+    </div>
+    <div class="cronometro">
+        <span class="minutos">01</span>:<span class="segundos">00</span>
+    </div>
+
+    <div id="div-erros" class="box bg-principal">
+        <img class="pilhas" id="pilha-1" src="images/bateria.png">
+        <img class="pilhas" id="pilha-2" src="images/bateria.png">
+        <img class="pilhas" id="pilha-3" src="images/bateria.png">
+    </div>
+</div>
