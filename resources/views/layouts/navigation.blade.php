b8cba5b50bdd265c6ba0fdaaae6b8f79f06b11dd
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-29 22:04:16 -0300

    Update navigation.blade.php
    correção de rotas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('jogo.show')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('jogo.show')" :active="request()->routeIs('jogo.show')">
                         {{ __('Jogo') }}
                     </x-nav-link>
                     <x-nav-link :href="route('partida.show')" :active="request()->routeIs('partida.show')">
@@ -70,8 +70,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('jogo.show')" :active="request()->routeIs('jogo.show')">
+                {{ __('Jogo') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('partida.show')" :active="request()->routeIs('partida.show')">
+                {{ __('Ranking') }}
             </x-responsive-nav-link>
         </div>
 
