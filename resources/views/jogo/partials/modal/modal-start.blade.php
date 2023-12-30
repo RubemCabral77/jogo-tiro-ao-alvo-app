1781ad6c8b63c475a5a3b867d04e9805aecf368e
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-29 22:00:11 -0300

    Create modal-start.blade.php
    criação do modal start

---
+++ resources/views/jogo/partials/modal/modal-start.blade.php
@@ -0,0 +1,8 @@
+<div class="modal" id="modal-start">
+    <div class="div-conteudo-modal">
+        <h2 class="texto-modal">Detone o Caruso!</h2>
+        <div class="div-btn-start">
+            <button class="btn-jogo" id="btn-start">JOGAR</button>
+        </div>
+    </div>
+</div>
