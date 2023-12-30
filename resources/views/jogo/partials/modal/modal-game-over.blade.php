466333255a7d2a53274a97d609f68ba55e5c1ad0
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-29 22:00:29 -0300

    Create modal-game-over.blade.php
    criação do modal game over

---
+++ resources/views/jogo/partials/modal/modal-game-over.blade.php
@@ -0,0 +1,9 @@
+<div class="modal" id="modal-game-over">
+    <div class="div-conteudo-modal">
+        <h2 class="texto-modal">Game Over</h2>
+        @include('jogo.partials.modal.form')
+        <h3 class="">Pontuação: <span id="span-pontuacao"></span></h3>
+        <h3 class="">Tentativas: <span id="span-tentativas"></span></h3>
+    </div>
+</div>
+<br>
