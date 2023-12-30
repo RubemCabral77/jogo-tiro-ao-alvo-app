3aa3133e1298e2dc516b01722b61b74e8f4691e3
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-30 16:10:07 -0300

    Update web.php
    correção de erro de login

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
