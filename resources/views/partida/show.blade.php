cad16b48e5acd2f5b1af996477157c58929dedd6
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-29 22:04:45 -0300

    Update show.blade.php
    modificação de espaços da tabela do ranking

--- resources/views/partida/show.blade.php
+++ resources/views/partida/show.blade.php
@@ -19,9 +19,9 @@
                         @else
                         <tr>
                             <th class="text-start p-2 text-lg border-none m-0 rounded-tl-2xl" style="background-color: #3437f7; color: #dde15b;">JOGADOR</th>
-                            <th class="text-start p-2 text-lg border-none m-0" style="background-color: #3437f7; color: #dde15b;">QUANTIDADE DE ACERTOS</th>
-                            <th class="text-start p-2 text-lg border-none m-0" style="background-color: #3437f7; color: #dde15b;">QUANTIDADE DE ERROS</th>
-                            <th class="text-start p-2 text-lg border-none m-0 rounded-tr-2xl" style="background-color: #3437f7; color: #dde15b;">DATA E HORA</th>
+                            <th class="text-start p-2 text-lg border-none m-0" style="background-color: #3437f7; color: #dde15b;">ACERTOS</th>
+                            <th class="text-start p-2 text-lg border-none m-0" style="background-color: #3437f7; color: #dde15b;">ERROS</th>
+                            <th class="text-start p-2 text-lg border-none m-0 rounded-tr-2xl" style="background-color: #3437f7; color: #dde15b;">DATA-HORA</th>
                         </tr>
                         @endif
                         
