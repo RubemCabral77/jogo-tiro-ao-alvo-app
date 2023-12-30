a9d6e3d4270d35ffd9c31300cd1334ead3bbe784
Rubem Cabral <rubemsenhorrubem@gmail.com>
2023-12-29 22:01:15 -0300

    Update punctuation.blade.php
    adaptação de css para tailwind

--- resources/views/jogo/partials/punctuation.blade.php
+++ resources/views/jogo/partials/punctuation.blade.php
@@ -1,14 +1,14 @@
-<div class="classificacao row mt1 mb1 espaco-ao-redor">
-    <div class="box bg-principal">
-        <h2 class="col text-center">Acertos: <span id="acertos">0</span></h2>
+<div class="classificacao flex flex-row flex-nowrap mt-4 mb-4 justify-around">
+    <div class="p-4 rounded-md bg-principal">
+        <h2 class="flex flex-col flex-nowrap text-center">Acertos: <span id="acertos" class="text-lg font-semibold">0</span></h2>
     </div>
-    <div class="cronometro">
-        <span class="minutos">01</span>:<span class="segundos">00</span>
+    <div class="w-10 flex justify-center items-center cronometro">
+        <span class="minutos text-lg font-semibold">01</span>:<span class="segundos text-lg font-semibold">00</span>
     </div>
 
-    <div id="div-erros" class="box bg-principal">
-        <img class="pilhas" id="pilha-1" src="images/bateria.png">
-        <img class="pilhas" id="pilha-2" src="images/bateria.png">
-        <img class="pilhas" id="pilha-3" src="images/bateria.png">
+    <div id="div-erros" class="p-4 rounded-md bg-principal w-10 flex justify-center items-center">
+        <img class="pilhas w-30" id="pilha-1" src="{{ asset('images/bateria.png') }}" >
+        <img class="pilhas w-30" id="pilha-2" src="{{ asset('images/bateria.png') }}">
+        <img class="pilhas w-30" id="pilha-3" src="{{ asset('images/bateria.png') }}">
     </div>
 </div>
